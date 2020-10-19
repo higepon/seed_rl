b1d075d12a10492d03f835b2b1b374c6712b97d7
Taro Minowa <higepon@gmail.com>
2020-10-19 17:10:15 +0900

    adaptive difficulty

--- football/vtrace_adaptive_main.py
+++ football/vtrace_adaptive_main.py
@@ -50,7 +50,7 @@ class DifficultyWrapper(gym.Wrapper):
     if done:
         raw_reward = float(info['score_reward'])
         self.raw_rewards.append(raw_reward)
-        print(f"game_reward={info['score_reward']} avg_raw_reward={np.mean(self.raw_rewards)} {self.raw_rewards}", file=sys.stderr)
+        print(f"info={info} game_reward={info['score_reward']} avg_raw_reward={np.mean(self.raw_rewards)} {self.raw_rewards}", file=sys.stderr)
         if len(self.raw_rewards) == 3 and np.mean(self.raw_rewards) >= 1.1:
             self.unwrapped._config.ScenarioConfig().right_team_difficulty += 0.001
             print(f"**** difficulty increased to {self.unwrapped._config.ScenarioConfig().right_team_difficulty}", file=sys.stderr)
