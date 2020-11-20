36b9aa490d57ce8e2cbc638901a412f146727d95
Taro Minowa <higepon@gmail.com>
2020-11-20 14:15:39 +0900

    fix

--- common/actor.py
+++ common/actor.py
@@ -29,6 +29,7 @@ from seed_rl.common import utils
 from seed_rl.common import bot
 import tensorflow as tf
 import traceback
+import collections
 
 
 FLAGS = flags.FLAGS
@@ -56,6 +57,62 @@ def number_of_actors():
       except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
           pass
   return num
+
+SMM_WIDTH = 96
+SMM_HEIGHT = 72
+# Normalized minimap coordinates
+MINIMAP_NORM_X_MIN = -1.0
+MINIMAP_NORM_X_MAX = 1.0
+MINIMAP_NORM_Y_MIN = -1.0 / 2.25
+MINIMAP_NORM_Y_MAX = 1.0 / 2.25
+_MARKER_VALUE = 255
+SMM_LAYERS = ['left_team', 'right_team', 'ball', 'active']
+
+def get_smm_layers(config):
+  return SMM_LAYERS
+
+def mark_points(frame, points):
+  """Draw dots corresponding to 'points'.
+  Args:
+    frame: 2-d matrix representing one SMM channel ([y, x])
+    points: a list of (x, y) coordinates to be marked
+  """
+  for p in range(len(points) // 2):
+    x = int((points[p * 2] - MINIMAP_NORM_X_MIN) /
+            (MINIMAP_NORM_X_MAX - MINIMAP_NORM_X_MIN) * frame.shape[1])
+    y = int((points[p * 2 + 1] - MINIMAP_NORM_Y_MIN) /
+            (MINIMAP_NORM_Y_MAX - MINIMAP_NORM_Y_MIN) * frame.shape[0])
+    x = max(0, min(frame.shape[1] - 1, x))
+    y = max(0, min(frame.shape[0] - 1, y))
+    frame[y, x] = _MARKER_VALUE
+
+def generate_smm(observation, config=None,
+                 channel_dimensions=(SMM_WIDTH, SMM_HEIGHT)):
+  """Returns a list of minimap observations given the raw features for each
+  active player.
+  Args:
+    observation: raw features from the environment
+    config: environment config
+    channel_dimensions: resolution of SMM to generate
+  Returns:
+    (N, H, W, C) - shaped np array representing SMM. N stands for the number of
+    players we are controlling.
+  """
+  frame = np.zeros((len(observation), channel_dimensions[1],
+                    channel_dimensions[0], len(get_smm_layers(config))),
+                   dtype=np.uint8)
+
+  for o_i, o in enumerate(observation):
+    for index, layer in enumerate(get_smm_layers(config)):
+      assert layer in o
+      if layer == 'active':
+        if o[layer] == -1:
+          continue
+        mark_points(frame[o_i, :, :, index],
+                    np.array(o['left_team'][o[layer]]).reshape(-1))
+      else:
+        mark_points(frame[o_i, :, :, index], np.array(o[layer]).reshape(-1))
+  return frame
 
 def actor_loop(create_env_fn):
   """Main actor loop.
@@ -88,6 +145,7 @@ def actor_loop(create_env_fn):
         # Unique ID to identify a specific run of an actor.
         run_id = np.random.randint(np.iinfo(np.int64).max)
         observation, observation2 = env.reset()
+        observations = collections.deque([], maxlen=4)
         reward = 0.0
         raw_reward = 0.0
         done = False
@@ -107,6 +165,16 @@ def actor_loop(create_env_fn):
 
           tf.summary.experimental.set_step(actor_step)
           print("***obs", observation, file=sys.stderr)
+
+          # create SMM stacked
+          observation = observation['players_raw'][0]
+          generate_smm([observation])[0]
+          if not observations:
+              observations.extend([observation] * 4)
+          else:
+              observations.append(observation)
+          observation = np.concatenate(list(observations), axis=-1)
+
           env_output = utils.EnvOutput(reward, done, observation,
                                        abandoned, episode_step)
           with elapsed_inference_s_timer:
